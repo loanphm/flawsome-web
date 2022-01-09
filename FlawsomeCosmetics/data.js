4d2dcb203523514ec22cd30edba44a9920034637
loanphm288 <ploanloan5050@gmail.com>
2022-01-09 21:37:49 +0700

    fix code blog 0901

---
+++ FlawsomeCosmetics/data.js
@@ -0,0 +1,56 @@
+﻿const dataModel = [
+    {
+        "id": 1,
+        "name": 'Son dưỡng Yves Rocher Cerise',
+        "idCategory": 4, 
+        "image": 'ảnh son dưỡng lớn 1',
+        "image1": 'ảnh son dưỡng 1_1',
+        "image2": 'ảnh son dưỡng 1_2',
+        "image3": 'ảnh son dưỡng 1_3',
+        "price": 100000,
+        "descripton": 'Mùi hương ngọt ngào & nóng bỏng của quả Anh đào khiến đôi môi của bạn mềm mại hơn & được thể hiện bằng một ánh sáng màu đỏ bóng loáng.',
+        "oldPrice": '150000đ', 
+        "vote": 2, 
+        "brand": 'Yves Rocher',
+        "categoryName": 'Chăm sóc môi'
+    },
+
+     {
+        "id": 2,
+         "name": 'Son dưỡng Yves Rocher Vanille',
+        "idCategory": 4, 
+         "image": 'ảnh son dưỡng lớn 2',
+         "image1": 'ảnh son dưỡng 2_1',
+         "image2": 'ảnh son dưỡng 2_2',
+         "image3": 'ảnh son dưỡng 2_3',
+        "price": 100000, 
+         "descripton": 'Son chứa chiết xuất va ni - Vanilla chứa nhiều chất chống oxy hoá cao, giúp bảo vệ da khỏi những tác động xấu và mùi hương dịu nhẹ, thư giãn, giảm stress ngay tức thì',
+        "oldPrice": '150000đ', 
+        "vote": 5, 
+        "brand": 'Yves Rocher',
+        "categoryName": 'Chăm sóc môi'
+    },
+
+      {
+        "id": 3,
+          "name": 'Son dưỡng Yves Rocher Karité',
+        "idCategory": 4, 
+          "image": 'ảnh son dưỡng lớn 3',
+          "image1": 'ảnh son dưỡng 3_1',
+          "image2": 'ảnh son dưỡng 3_2',
+          "image3": 'ảnh son dưỡng 3_3',
+        "price": 100000, 
+          "descripton": 'Chất kem nhẹ được nhuộm trong một tông màu berry tinh tế và do đó nhấn mạnh hình dạng tự nhiên của đôi môi của bạn.',
+        "oldPrice": '150000đ',
+        "vote": 5, 
+        "brand": 'Yves Rocher',
+        "categoryName": 'Chăm sóc môi'
+    }
+]
+
+const categories = [
+    {
+        "id": 4,
+        "name": 'Son dưỡng'
+    }
+]
