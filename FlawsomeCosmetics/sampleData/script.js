6c12f1dc85f5f0ea2751644dbb9a8702d848f6e5
loanphm288 <ploanloan5050@gmail.com>
2022-01-15 00:49:39 +0700

    Fix footer, add code load XML (loi khong hien thong tin)

--- FlawsomeCosmetics/sampleData/script.js
+++ FlawsomeCosmetics/sampleData/script.js
@@ -1,13 +1,26 @@
-function loadData() {
-    $.ajax( {
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "member.xml", true);
+var html = "";
+var mem = document.getElementById("mem");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var xmlDoc = xhr.responseXML;
 
-        URL:"member.xml",
-        dataType: "xml",
-        success: function(data) {
-            $("ul").children().remove();
-            $(data).find("member").each (function () {
-                var
-            })
+        html = "<table border='1'>";
+        var x = xmlDoc.getElementsByTagName("sinhvien");
+        for (i = 0; i < x.length; i++) {
+            html += "<tr>";
+            html += "<td>" +
+                x[i].getElementsByTagName("ten")[0].childNodes[0].nodeValue + "</td>";
+            html += "<td>" +
+                x[i].getElementsByTagName("mssv")[0].childNodes[0].nodeValue + "</td>";
+
+            html += "</tr>";
         }
-     })
+        html += "</table>";
+    } else {
+        html += "Không lấy được dữ liệu";
+    }
+    mem.innerHTML = html;
 }
+xhr.send();
